d24e4c46797141c9b9cb5ebb276aa0019d647205
gawlster <connorgawley@gmail.com>
2024-05-02 21:17:17 -0700

    remove all files

--- lua/gawlster/autocommands.lua
+++
@@ -1,8 +0,0 @@
-local cmd = vim.cmd
-
-cmd([[
-augroup Mkdir
-  autocmd!
-  autocmd BufWritePre * call mkdir(expand("<afile>:p:h"), "p")
-augroup END
-]])
--- lua/gawlster/lsp/init.lua
+++
@@ -1,9 +0,0 @@
-local status_ok, _ = pcall(require, "lspconfig")
-if not status_ok then
-	return
-end
-
-require("gawlster.lsp.mason")
-require("gawlster.lsp.handlers").setup()
-require("gawlster.lsp.null-ls")
-require("gawlster.lsp.prettier")
