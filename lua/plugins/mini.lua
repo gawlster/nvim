92c42e63b5834eef4a0eca49697fd05dd4b99e92
gawlster <connorgawley@gmail.com>
2025-04-22 14:27:09 -0700

    Whichkey, telescope, toggleterm

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -2,6 +2,11 @@ return {
 	'echasnovski/mini.nvim',
 	version = false,
 	config = function()
+		-- icons
+		require("mini.icons").setup({})
+
+		-- files
 		require("mini.files").setup({})
+		vim.keymap.set("n", "<leader>e", "<cmd>lua MiniFiles.open()<CR>", {desc = "Open file explorer"})
 	end,
 }
